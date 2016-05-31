7947e991dc1fb57832ab02a2503304e3e2c84cfb
legion <1931330336@qq.com>
2016-05-31 19:53:57 +0800

    using variables

--- lvs/read_server.sh
+++ lvs/read_server.sh
@@ -2,21 +2,22 @@
 VIP=192.168.122.100
 case $1 in
     start)
-        ip addr add 192.168.122.100/32 dev lo
-        ip route add 192.168.122.100/32 dev lo
+	echo "Virtual Ipaddress is ${VIP}"
+        ip addr add ${VIP}/32 dev lo
+        ip route add ${VIP}/32 dev lo
         echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore
         echo 2 > /proc/sys/net/ipv4/conf/lo/arp_announce
         echo 1 > /proc/sys/net/ipv4/conf/all/arp_ignore
         echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce;;
     stop)
-        ip add del 192.168.122.100/32 dev lo
-        ip route del 192.168.122.100/32 dev lo
+        ip add del ${VIP}/32 dev lo
+        ip route del ${VIP}/32 dev lo
         echo 0 > /proc/sys/net/ipv4/conf/lo/arp_ignore
         echo 0 > /proc/sys/net/ipv4/conf/lo/arp_announce
         echo 0 > /proc/sys/net/ipv4/conf/all/arp_ignore
         echo 0 > /proc/sys/net/ipv4/conf/all/arp_announce;;
        *)
-        echo 'Usage $0 start|stop'
+        echo 'Usaga: $0 {start|stop}'
         exit 0;;
 esac
 
